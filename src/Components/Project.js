7ee54e6f17be6f411f12943a4343a3112f51ae93
Raul Lopez Luna <raullopez2094@gmail.com>
2018-02-27 14:37:02 -0800

    added styling to each project

--- src/Components/Project.js
+++ src/Components/Project.js
@@ -4,21 +4,28 @@ import '../Styles/Project.css'
 // TODO: replace alt with value
 export default function Project(props){
   function getTech(){
-    return props.tech.map( tech => {
-      return <span key={tech}> {tech} </span>
+    return props.tech.map( (item, index) => {
+      return <span key={item}>
+        {item + (index === props.tech.length -1 ? " " : " - ")}
+      </span>
     })
   }
-        return(
-        <div className="project">
-          {/* <img src="" alt="placeholder"/> */}
-          <h2>{props.title}</h2>
-          <p>{props.description}</p>
-          <p>{props.role}</p>
-          <div>
-            {getTech()}
-          </div>
-          <p>{props.note}</p>
-          <a href={props.link}>Check out the project</a>
+    return(
+    <div className="project">
+      {/* <img src="" alt="placeholder"/> */}
+      {/* <p className="title">{props.title}</p> */}
+      <div className="title">
+        <h3>{props.title}</h3>
+        <p>{props.role}</p>
+        <p>{props.note}</p>
+      </div>
+      <div className="content">
+        <p>{props.description}</p>
+        <div>
+          {getTech()}
         </div>
+        <a href={props.link}>Check out the project</a>
+      </div>
+    </div>
   )
 }
--- src/Components/ProjectWrap.js
+++ src/Components/ProjectWrap.js
@@ -10,16 +10,17 @@ export default function ProjectWrap(){
       description:`Worked with a front end and back end team to implement a full
       stack application. `,
       role:"Front End Developer",
-      tech: ["js", 'vue' ],
+      tech: ["js", 'Vue' ],
       note: "CMPS 116-117 - Senior Project",
       link: ""
     },
     {
       title: "Quest Journal",
       img:"",
-      description:"",
+      description:`Java programmer in group of 3. Helped design all parts of the app
+      including UI, database, and flow between app pages.`,
       role:"Android Dev",
-      tech: ["Android Studio", 'Firebase' ],
+      tech: ["Android Studio", 'Firebase', 'Java'],
       note: "CMPS 121 - Mobile Apps",
       link: "https://github.com/jasonou/Quest-Journal"
     },
@@ -44,10 +45,17 @@ export default function ProjectWrap(){
     {
       title: "League Of Legends Match History",
       img:"",
-      description:"",
-      note:"Side Project",
+      // description:`Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+      // do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+      // minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex
+      // ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+      // velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+      // cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum`,
+      // note:"Side Project",
+      description:`Match history for powered by Riot Games Api. Different from other
+      match history websites because `,
       role:"Web Dev",
-      tech: ["React", 'express', 'nodejs' ],
+      tech: ["React", 'Express', 'Nodejs' ],
       link:"https://rlopezlu.github.io/league-history/"
     },
     {
@@ -57,7 +65,7 @@ export default function ProjectWrap(){
       typing test.`,
       note:"Side Project",
       role:"Web Dev",
-      tech: ["React", 'express', 'nodejs' ],
+      tech: ["React", 'Express', 'Nodejs' ],
       link:"https://rlopezlu.github.io/typing-speed-test/"
     },
     {
@@ -67,26 +75,30 @@ export default function ProjectWrap(){
       current weather.`,
       note:"Side Project",
       role:"Web Dev",
-      tech: ["Twilio", 'express', 'nodejs' ],
+      tech: ["Twilio", 'Express', 'Nodejs' ],
       link:"https://github.com/rlopezlu/TwilioWeather"
     }
   ]
 
   return(
-    <div className="project-wrap">
-      {myProjects.map( project => {
-        return <Project
-          key={project.title}
-          title={project.title}
-          img={project.img}
-          description={project.description}
-          role={project.role}
-          tech={project.tech}
-          note={project.note}
-          link={project.link}
-               />
-      })
-      }
+    <div>
+      <h2>Projects</h2>
+      <div className="project-wrap">
+
+        {myProjects.map( project => {
+          return <Project
+            key={project.title}
+            title={project.title}
+            img={project.img}
+            description={project.description}
+            role={project.role}
+            tech={project.tech}
+            note={project.note}
+            link={project.link}
+                 />
+        })
+        }
+      </div>
     </div>
   )
 }
